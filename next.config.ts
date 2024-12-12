2e0fa9e5412316759db40dbf83a8153bf6a4ee03
Chandler Casey <chandlercasey13@gmail.com>
2024-12-12 11:28:19 -0600

    Add placeholder images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  images: {
+    domains: ['www.aplaceformom.com', 'cdn.brookfieldresidential.net'],  // Add the domain of the external image
+  },
 };
 
 export default nextConfig;
