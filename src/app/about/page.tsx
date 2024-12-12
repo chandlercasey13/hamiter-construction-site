935d57f49af18e9ff5c5ddb3fcdd44202c6dc918
Chandler Casey <chandlercasey13@gmail.com>
2024-12-12 08:28:17 -0600

    Remove next config

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,11 @@
-export default function Home() {
+export default function About() {
     return (
       <div className="flex flex-col items-center justify-center min-h-[calc(100vh-4rem)]">
-        <h1 className="text-4xl font-bold mb-4 text-center">Hamiter Construction</h1>
+        <h1 className="text-4xl font-bold mb-4 text-center">About</h1>
         <p className="text-xl text-center">
-         
+        
           <br />
-         
+          
         </p>
       </div>
     )
