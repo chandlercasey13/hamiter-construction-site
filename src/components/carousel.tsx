dce25481204763328e00297236ec74874ebdcbc3
Chandler Casey <chandlercasey13@gmail.com>
2024-12-13 09:33:38 -0600

    Remove unused button

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
-import { Button } from "@/components/ui/button"
+
 interface CarouselProps {
   images: string[]
   interval?: number
