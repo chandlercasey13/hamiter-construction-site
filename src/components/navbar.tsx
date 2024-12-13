679626e6e69ebf7d89165bfa797571e78207d80a
Chandler Casey <chandlercasey13@gmail.com>
2024-12-13 12:24:44 -0600

    Remove React Link dep

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="max-w-7xl  mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-14">
           <div className="flex-shrink-0 flex items-center">
-            <Link href="/" className="text-xl font-bold text-gray-800">
+            <Link href="/" className="text-lg font-semibold text-gray-800">
               HC
             </Link>
           </div>
